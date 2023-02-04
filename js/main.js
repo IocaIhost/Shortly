64352811595d2a414da1e9ec1685655268f5da18
DevOps Engineer <rapbrand06112000@gmail.com>
2023-02-04 15:30:17 +0500

    Add URL copy button click handler and wrap form submit handler in if statement

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 const elUrlShortenerFomr = document.querySelector('.js-url-shortener__form');
 const elUrlShortenerRes = document.querySelector('.url-shortener__results');
 
-elUrlShortenerFomr.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    elUrlShortenerRes.classList.add("url-shortener__results--opener");
-})
+
+if (elUrlShortenerFomr) {
+    elUrlShortenerFomr.addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        elUrlShortenerRes.classList.add("url-shortener__results--opener");
+    })
+}
+
+const elUrlShortener = document.querySelector('.url-shortener');
+const elCopyUrlShortLinkButton = document.querySelector('.js-copy-short-link-button');
+const urlShortLink  = document.querySelector(".url-shortener__short-link");
+
+if (elUrlShortener) {
+    elUrlShortener.addEventListener("click", function (evt) {
+        if (evt.target.matches(".js-copy-short-link-button")) {
+            // Change button test
+            evt.target.textContent = "Copied";
+
+            // Change color
+            evt.target.classList.add("url-shortener__copy-button--copied");
+
+            // Reset btn text and class after one second
+            setTimeout(function () {
+                evt.target.textContent = "Copy";
+                evt.target.classList.remove("url-shortener__copy-button--copied");
+            }, 2000);
+        }
+    })
+}
